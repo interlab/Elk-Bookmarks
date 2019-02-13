b663edf0f537a7a2943ed81d62b66580c6e41b27
Spuds <spuds@spudsdesign.com>
2019-02-13 10:35:48 -0600

    ! add German translation

---
+++ theme/Bookmarks.german.php
@@ -0,0 +1,18 @@
+<?php
+
+$txt['bookmarks_enabled'] = 'Aktiviere Bookmarks';
+$txt['bookmarks'] = 'Meine Bookmarks';
+$txt['bookmark_list'] = 'Bookmarks';
+$txt['bookmark'] = 'Bookmark';
+$txt['bookmark_list_empty'] = 'Derzeit hast du keine Bookmarks gesetzt. Setze Bookmarks, indem du \'Setze Bookmark\' in Themen deiner Wahl anklickst.';
+$txt['bookmark_open_window'] = 'In neuem Fenster öffnen';
+$txt['bookmark_delete'] = 'Lösche markierte Bookmarks';
+$txt['bookmark_delete_success'] = '%1$s Bookmarks wurden erfolgreich gelöscht!';
+$txt['bookmark_delete_failure'] = 'Es wurden keine Bookmarks gelöscht.';
+$txt['bookmark_add'] = 'Setze Bookmark';
+$txt['bookmark_delete'] = 'Entferne Bookmark';
+$txt['bookmark_add_success'] = 'Dieses Thema wurde erfolgreich zu deinen Bookmarks hinzugefügt!';
+$txt['bookmark_add_exists'] = 'Dieses Thema ist bereits als Bookmark gesetzt!';
+$txt['bookmark_add_failed'] = 'Oops, es ist ein Fehler aufgetreten...';
+$txt['permissionname_make_bookmarks'] = 'Darf Bookmarks zu Themen setzen';
+$txt['cannot_make_bookmarks'] = 'Entschuldige, du besitzt nicht die Berechtigung, Bookmarks zu setzen.';
