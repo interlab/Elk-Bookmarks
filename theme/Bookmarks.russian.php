814fae0ded3ba0eed919625956333a06ed5aec0f
inter <ytkin3000@gmail.com>
2021-04-29 23:02:45 +0300

    update translation

--- theme/Bookmarks.russian.php
+++ theme/Bookmarks.russian.php
@@ -1,18 +1,18 @@
 <?php
 
-$txt['bookmarks_enabled'] = 'Включить избранное';
+$txt['bookmarks_enabled'] = 'Включить закладки';
 $txt['bookmarks'] = 'Избранное';
-$txt['bookmark_list'] = 'Избранное';
+$txt['bookmark_list'] = 'Закладки';
 $txt['bookmark'] = 'Избранное';
-$txt['bookmark_list_empty'] = 'На данный момент избранное пусто. Добавьте в избранное, нажав кнопку "Избранное" в теме.';
+$txt['bookmark_list_empty'] = 'На данный момент список пуст. Добавьте закладку, нажав кнопку "Избранное" в теме.';
 $txt['bookmark_open_window'] = 'Открыть в новом окне';
-$txt['bookmark_delete'] = 'Удалить выбранные темы из избранного';
-$txt['bookmark_delete_success'] = '%1$s тем из избранного были успешно удалены!';
-$txt['bookmark_delete_failure'] = 'Темы из избранного не были удалены.';
-$txt['bookmark_add'] = 'Избранное';
+$txt['bookmark_delete'] = 'Удалить выбранные закладки';
+$txt['bookmark_delete_success'] = '%1$s закладок были успешно удалены!';
+$txt['bookmark_delete_failure'] = 'Закладки не были удалены.';
+$txt['bookmark_add'] = 'Добавить закладку';
 $txt['bookmark_delete'] = 'Удалить из избранного';
 $txt['bookmark_add_success'] = 'Тема была успешно добавлена в избранное!';
 $txt['bookmark_add_exists'] = 'Эта тема уже есть в избранном!';
 $txt['bookmark_add_failed'] = 'Не удалось добавить тему в избранное.';
-$txt['permissionname_make_bookmarks'] = 'Создание тем в избранном';
-$txt['cannot_make_bookmarks'] = 'Извините, у вас нет разрешения на создание в избранном.';
+$txt['permissionname_make_bookmarks'] = 'Создание закладок';
+$txt['cannot_make_bookmarks'] = 'Извините, у вас нет разрешения на создание закладок.';
